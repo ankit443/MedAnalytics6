d20eb94004193444006b42f412ab6d2a2082dc4b
Ankit Deb <deb.ankit.work@gmail.com>
2023-03-03 13:03:24 +0530

    started working on dashboard page for App

---
+++ app/src/main/java/com/epics/medanalytics6/DashboardMain.java
@@ -0,0 +1,14 @@
+package com.epics.medanalytics6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DashboardMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard_main);
+    }
+}
